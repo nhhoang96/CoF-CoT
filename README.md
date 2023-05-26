df6067f0e484653265dfd08fed865cfcc58f856a
nhhoang96 <hnguy7@uic.edu>
2023-05-26 01:46:47 -0500

    Add Demos

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # llm_nlu
 Put the evaluating file under ./gpt_output and change the variable file_name in cal_metric.py to run the evaluation
+
+# Additional Note (May 25th)
+(1) Run exp for CoT (no conditioning)
+```
+python query_to_lf.py --type_condition='none' --add_demo='false'
+```
+
+(2) Run exp for CoT (conditioning)
+```
+python query_to_lf.py --type_condition='control' --add_demo='false'
+```
+
+(3) Run exp for CoT (with demos-no conditioning)
+```
+python query_to_lf.py --type_condition='none' --add_demo='true'
+```
+
--- query_to_lf.py
+++ query_to_lf.py
@@ -7,10 +7,11 @@ import json
 import os
 import time
 import argparse
-import openai
+import re
+#import openai
 
-openai.api_key = os.getenv("openai_key")
-model_name = "gpt-3.5-turbo"
+#openai.api_key = os.getenv("openai_key")
+#model_name = "gpt-3.5-turbo"
 
 
 def get_intent_slot_vob(dataset):
@@ -40,6 +41,7 @@ def get_intent_slot_vob(dataset):
                         'subtract_time_timer', 'switch_call', 'unloop_music', 'update_alarm', 'update_call',
                         'update_method_call', 'update_reminder', 'update_reminder_date_time',
                         'update_reminder_location', 'update_reminder_todo', 'update_timer']
+
 
         slot_vocab = ['music_radio_id', 'alarm_name', 'recipes_diet', 'recipes_meal', 'group', 'attendee',
                       'music_artist_name', 'location', 'recipes_unit_nutrition', 'recipes_cooking_method',
@@ -102,16 +104,81 @@ def call_chatgpt(input_prompt):
     response = response["choices"][0]['message']['content'].strip()
     return response
 
+def parse_lf(lf):
+    #print ("LF", lf)
+    intent_slot = re.sub(r'[\[\]]',' ',lf).strip()
+    #print ("Type check", type(intent_slot))
+    #print ("Check item", repr(intent_slot))
+    intent_slot = re.sub('\s\s+', ' ', intent_slot)
+    intent_slot = intent_slot.split(' ')
+    slots = []
+    slot_pairs=[]
+    intent=''
+    slot_val=''
+    slot_vals=[]
+    for item in intent_slot:
+        item = item.replace('[','')
+        item = item.replace(']','')
+        if (item.startswith('IN:')):
+            intent = item.split(':')[-1].lower()
+        elif (item.startswith('SL:')): # slot type
+            if (len(slot_pairs) >= 1):
+                slot_pairs.append(slot_val.strip())
+                slot_vals.append(slot_val)
+                slots.append(tuple(slot_pairs))  
+                slot_pairs= []
+            slot_pairs.append(item.split(':')[-1].lower())
+
+            #reset
+            slot_val = ''
+        else:
+            slot_val+= ' ' + item
+
+    # Final slots
+    slot_pairs.append(slot_val.strip())
+    slot_vals.append(slot_val.strip())
+    slots.append(tuple(slot_pairs))
+    #print ("Extracted Intent: %s \t Slots: %s \n"%(intent, slots))
+    return intent, slots, slot_vals
 
 # ----- Output file definition ------#
 input_test_file = "nlu_data/mtop_flat_simple/en/eval.txt"
 with open(input_test_file, 'r') as file:
     content = file.read()
 
+
+input_fs_file = "nlu_data/mtop_flat_simple/en/fs.txt"
+
+demo_ex=[]
+ex_counter=0
+for line in open(input_fs_file, 'r'):
+    ex = line.strip().split('\t')
+    utt = ex[0]
+    lf = ex[1]
+    domain=ex[3]
+    amr_info = ex[-1] #when generated
+    print ("AMR INFO", amr_info)
+    intent, slot_pairs, slot_vals = parse_lf(lf)
+    demo_dict={'utt':'', 'intent':'', 'key_phrase':'', 'pair':'', 'AMR':'', 'lf':''}
+    demo_dict['utt'] = utt
+    demo_dict['intent'] = intent
+    demo_dict['pair'] = ','.join(map(str,slot_pairs))
+    demo_dict['key_phrase'] = ','.join(slot_vals)
+    demo_dict['AMR'] = amr_info
+    demo_dict['lf'] = lf
+
+    print ("Demo dict", demo_dict)
+    demo_ex.append(demo_dict)
+    ex_counter += 1
+    #break
+
+
+    
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", default="MTOP", choices=["MTOP", "MASSIVE"], type=str,
-                    help='Kind of conditioning: (none, condition)')
+                    help='Type of dataset')
 parser.add_argument("--type_condition", default='control', type=str, help='Kind of conditioning: (none, condition)')
+parser.add_argument("--add_demo", choices=['true','false'], default='false', type=str)
 args = parser.parse_args()
 
 # ---- Generic Structure
@@ -129,28 +196,35 @@ gen_step_2 = 'Based on the sentence and its AMR graph, identify a list of key ph
 
 gen_step_2c = 'Based on the sentence, its potential intents and its AMR graph, identify a list of key phrases for the sentence. Key phrases can be made up from multiple AMR concepts. Each word in key phrases must exist in the given sentence. Return a list of key phrases separated by commas \n'
 
-gen_step_3 = 'Given the slot vocabulary, the sentence and its key phrases, identify the corresponding slot type for each key phrases. Return the list of key phrases and their corresponding slot types in the following format: (key_phrase, slot_type) separated by commas \n'
+gen_step_3 = 'Given the slot vocabulary, the sentence and its key phrases, identify the corresponding slot type for each key phrases. Return the list of key phrases and their corresponding slot types in the following format: (slot_type, key_phrase) separated by commas \n'
 gen_step_3 += 'Slot Vocabulary: ' + slot_str + '\n'
 
-gen_step_3c = 'Given the slot vocabulary, the sentence, its potential intents and its key phrases, identify the corresponding slot type for each key phrases. Return the list of key phrases and their corresponding slot types in the following format: (key_phrase, slot_type) separated by commas \n'
+gen_step_3c = 'Given the slot vocabulary, the sentence, its potential intents and its key phrases, identify the corresponding slot type for each key phrases. Return the list of key phrases and their corresponding slot types in the following format: (slot_type, key_phrase) separated by commas \n'
 gen_step_3c += 'Slot Vocabulary: ' + slot_str + '\n'
 
-gen_step_4 = 'Given the sentence, its potential intent types, its slot type and slot value pairs in (slot_value, slot_type) format, generate the logic form in the format: [IN:___ [SL:___] [SL:____]] where IN: is followed by an intent type and SL: is followed by a slot type and slot value pair separated by white space. The number of [SL: ] is unlimited. The number of [IN: ] is limited to 1 \n'
+gen_step_4 = 'Given the sentence, its potential intent types, its slot type and slot value pairs in (slot_type, slot_value) format, generate the logic form in the format: [IN:___ [SL:___] [SL:____]] where IN: is followed by an intent type and SL: is followed by a slot type and slot value pair separated by white space. The number of [SL: ] is unlimited. The number of [IN: ] is limited to 1 \n'
 
 direct_prompt = "New Session: Given the intent type vocabulary, slot type vocabulary and sentence, generate logic form of the sentence in the format of [IN:___ [SL:____] [SL:___]] where IN: is followed by an intent type and SL: is followed by a slot type and slot value pair separated by white space. The number of [SL: ] is unlimited. \n"
 direct_prompt += "Intent Type: " + intent_str + "\n"
 direct_prompt += "Slot Type: " + slot_str + "\n"
 
 type_prompt = "chain_of"
-result_output_file = f'result/{type_prompt}_dialogue.jsonl'
+add_demo, condition_type = args.add_demo, args.type_condition
+result_output_file = f'result/{type_prompt}_demo_{add_demo}_condition_{condition_type}_dialogue.jsonl'
 writer = open(result_output_file, 'w')
+slot_type=''
+intent=''
+amr_graph=''
+key_phrases=''
+
 content = content.split("\n")
-for example in content[0:200]:
+for example in content[0:1]:
     utterance, logical_form, _, _, tag = example.split("\t")
     # --- Directly prompt
     if type_prompt == "direct":
         direct_prompt += "Sentence: " + utterance + "\n"
         direct_prompt += "Just generate the Logic Form: "
+        print ("Direct prompt", direct_prompt)
         pred_lf = call_chatgpt(direct_prompt)
         writer.write(json.dumps({"utterance": utterance, "pred_lf": pred_lf, "gold_lf": logical_form}) + '\n')
     else:
@@ -162,8 +236,25 @@ for example in content[0:200]:
         step_1a_prompt += 'Sentence: ' + utterance + '\n'
         step_1a_prompt += 'Intent type: ' + '\n'
 
+        # --- Demo Step 1a
+        if (args.add_demo == 'true'):
+            if (args.type_condition == 'none'):
+                demo_1 = gen_step_1 + 'Intent Vocabulary: ' + intent_str + '\n'
+            else:
+                demo_1 = gen_step_1c + 'Intent Vocabulary: ' + intent_str + '\n'
+            for dem in demo_ex:
+                demo_utt = dem['utt']
+                demo_intent = dem['intent']
+                #print ("Demo ex", utt)
+
+                demo_1 += 'Sentence: ' + demo_utt + '\n'
+                demo_1 += 'Intent type: ' + demo_intent + '\n'
+
+            step_1a_prompt = demo_1 + '\n' + step_1a_prompt
+
         intent = call_chatgpt(step_1a_prompt)
         print("STEP 1a: Get Intent")
+        #print("STEP 1a: Get Intent", step_1a_prompt)
 
         # --- Step 1b: Get AMR Graph
         if (args.type_condition == 'none'):
@@ -171,42 +262,131 @@ for example in content[0:200]:
         else:
             step_1b_prompt = gen_step_1bc + 'Sentence: ' + utterance + '\n'
             step_1b_prompt += 'Potential Intent Types: ' + intent + '\n'
+        
+        step_1b_prompt += 'AMR Graph: ' + '\n'
+
+        #--- Demo 1b
+        if (args.add_demo == 'true'):
+            if (args.type_condition == 'none'):
+                demo_1b = gen_step_1b + 'Sentence: ' + demo_utt + '\n'
+            else:
+                demo_1b = gen_step_1bc + 'Sentence: ' + demo_utt + '\n'
+            for dem in demo_ex:
+                demo_utt = dem['utt']
+                demo_intent = dem['intent']
+                if (args.type_condition != 'none'):
+                    demo_1b += 'Potential Intent Types: ' + demo_intent + '\n'
+                
+                demo_1b += 'AMR Graph: ' + '\n'
+
+            step_1b_prompt  = demo_1b + '\n' + step_1b_prompt 
 
         amr_graph = call_chatgpt(step_1b_prompt)
         print("STEP 1b: Get AMR Graph")
+        #print("STEP 1b: Get AMR Graph", step_1b_prompt)
 
         # --- Step 2: Get Key Phrases
         if (args.type_condition == 'none'):
-            step_2 = gen_step_2 + 'Sentence: ' + utterance + '\n'
+            step_2_prompt = gen_step_2 + 'Sentence: ' + utterance + '\n'
         else:
+            step_2_prompt = gen_step_2c + 'Sentence: ' + utterance + '\n'
+            step_2_prompt += 'Potential Intents: ' + '\n'
 
-            step_2 = gen_step_2c + 'Sentence: ' + utterance + '\n'
-            step_2 += 'Potential Intents: ' + '\n'
 
-        step_2 += 'AMR Graph: ' + amr_graph + '\n'
-        step_2 += 'Key phrases: \n'
+        step_2_prompt += 'AMR Graph: ' + amr_graph + '\n'
+        step_2_prompt += 'Key phrases: \n'
+
+        # -- Step 2 Demo
+        if (args.add_demo == 'true'):
+            if (args.type_condition == 'none'):
+                demo_2 = gen_step_2 + 'Sentence: ' + demo_utt + '\n'
+            else:
+                demo_2 = gen_step_2c + 'Sentence: ' + demo_utt + '\n'
+        
+            for dem in demo_ex:
+                demo_utt = dem['utt']
+                demo_intent = dem['intent']
+                demo_kp = dem['key_phrase']
+                demo_amr = dem['AMR']
+                if (args.type_condition != 'none'):
+                    demo_2 += 'Potential Intents: ' + '\n'
+                demo_2 += 'AMR Graph: ' + demo_amr + '\n'
+                demo_2 += 'Key phrases: ' + demo_kp + '\n'
+
+            step_2_prompt  = demo_2 + '\n' + step_2_prompt 
+
+
         key_phrases = call_chatgpt(step_2)
         print("STEP 2: Get Key Phrases")
+        #print("STEP 2: Get Key Phrases", step_2_prompt)
 
         # --- Step 3: Get Slot Type
         if (args.type_condition == 'none'):
-            step_3 = gen_step_3 + 'Key phrases: ' + key_phrases + '\n'
-            step_3 += 'Sentence: ' + utterance + '\n'
+            step_3_prompt = gen_step_3 + 'Sentence: ' + utterance + '\n'
+            step_3_prompt += 'Key phrases: ' + key_phrases + '\n'
+            #step_3 = gen_step_3 + 'Key phrases: ' + key_phrases + '\n'
+            #step_3 += 'Sentence: ' + utterance + '\n'
         else:
-            step_3 = gen_step_3 + 'Key phrases: ' + '\n'
-            step_3 += 'Sentence: ' + utterance + '\n'
-            step_3 += 'Potential Intent Types: ' + intent + '\n'
+            step_3_prompt = gen_step_3c + 'Sentence: ' + utterance + '\n'
+            step_3_prompt += 'Potential Intent Types: ' + intent + '\n'
+            step_3_prompt += 'Key phrases: ' + key_phrases + '\n'
+            #step_3 = gen_step_3 + 'Key phrases: ' + '\n'
+            #step_3 += 'Sentence: ' + utterance + '\n'
 
-        step_3 += 'Key phrase, Slot Type pairs: \n'
+        step_3_prompt += 'Slot Type, Key phrase  pairs: \n'
+
+
+        if (args.add_demo == 'true'):
+            if (args.type_condition == 'none'):
+                demo_3 = gen_step_3 + 'Sentence: ' + demo_utt + '\n'
+            else:
+                demo_3 = gen_step_3c + 'Sentence: ' + demo_utt + '\n'
+        
+            for dem in demo_ex:
+                demo_utt = dem['utt']
+                demo_intent = dem['intent']
+                demo_kp = dem['key_phrase']
+                demo_pair = dem['pair']
+                if (args.type_condition != 'none'):
+                    demo_3 += 'Potential Intents: ' + '\n'
+                demo_3 += 'AMR Graph: ' + demo_amr + '\n'
+                demo_3 += 'Key phrases: ' + demo_kp + '\n'
+
+                demo_3 += 'Slot Type, Key phrase pairs: ' + demo_pair + '\n'
+
+            step_3_prompt  = demo_3 + '\n' + step_3_prompt 
+
         slot_type = call_chatgpt(step_3)
         print("STEP 3: Get Slot Type")
+        #print("STEP 3: Get Slot Type", step_3_prompt)
+        
 
         # --- Step 4: Get Logic Form
-        step_4 = gen_step_4 + 'Sentence: ' + utterance + '\n'
-        step_4 += "Intent: " + intent + '\n'
-        step_4 += "Slot Type: " + slot_type + '\n'
-        step_4 += 'Logic Form: \n'
+        step_4_prompt = gen_step_4 + 'Sentence: ' + utterance + '\n'
+        step_4_prompt += "Intent: " + intent + '\n'
+        step_4_prompt += "Slot Type, Slot Value pairs: " + slot_type + '\n'
+        step_4_prompt += 'Logic Form: \n'
+
+
+        #--- Add Demo Step 4
+        if (args.add_demo == 'true'):
+            demo_4 = gen_step_4 + 'Sentence: ' + demo_utt + '\n'
+        
+            for dem in demo_ex:
+                demo_utt = dem['utt']
+                demo_intent = dem['intent']
+                demo_kp = dem['key_phrase']
+                demo_pair = dem['pair']
+
+                demo_4 += "Intent: " + demo_intent + '\n'
+                demo_4 += "Slot Type, Slot Value pairs: " + demo_pair + '\n'
+                demo_4 += 'Logic Form: ' + dem['lf'] + '\n'
+
+
+            step_4_prompt  = demo_4 + '\n' + step_4_prompt 
+
         pred_lf = call_chatgpt(step_4)
         print("STEP 4: Get Logic Form")
+        #print("STEP 4: Get Logic Form", step_4_prompt)
         writer.write(json.dumps({"utterance": utterance, "intent": intent, "AMR Graph": amr_graph, "key_phrase":
             key_phrases, "slot_type": slot_type, "pred_lf": pred_lf, "gold_lf": logical_form}) + '\n')
