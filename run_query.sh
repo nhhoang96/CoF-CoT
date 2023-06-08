c991e06344fb70fb0e0a4de7acd58f3c5966bf26
nhhoang96 <hnguy7@uic.edu>
2023-06-08 12:16:34 -0500

    run api

--- run_query.sh
+++ run_query.sh
@@ -1,7 +1,7 @@
 demo_file='demo_16_label'
 test_file='test_out_domain_seed'
 dataset='MTOP'
-output='test'
+output='api'
 seeds=(111 222 333)
 #python query_to_lf.py --type_condition='none' --add_demo='false' --output_for='test' --number_output=1 --structure_rep='none' --voting_method='complex' --number_demo=1 --demo_select_criteria='length' --condition_on='descr' --demo_file=${demo_file} --test_file=${test_file} --dataset=${dataset}
 
@@ -34,7 +34,7 @@ for s in ${seeds[@]}; do
 	python query_to_lf.py --type_condition='none' --add_demo='false' --output_for=${output} --number_output=10 --voting_method='complex' --dataset=${dataset} --test_file=${test_file} --seed=$s
 
 
-	#Few-shot:
+	#Few-shot: HOLD
 	# Baseline FS: SC-COT, COMPLEXCOT, RandomCOT (RetrieveCOT)
 	python query_to_lf.py --type_condition='none' --add_demo='true' --number_demo=16 --output_for=${output} --number_output=10 --voting_method='major' --demo_select_criteria='random' --dataset=${dataset} --test_file=${test_file} --seed=$s
 	python query_to_lf.py --type_condition='none' --add_demo='false' --number_demo=16 --output_for=${output} --number_output=10 --voting_method='complex' --demo_select_criteria='length' --dataset=${dataset} --test_file=${test_file} --seed=$s
